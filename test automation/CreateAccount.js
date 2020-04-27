21d97d95fd42c08c771d6b7506095b3d1d8cb8ca
Hyprono <31711635+Hyprono@users.noreply.github.com>
2020-04-27 16:45:39 -0700

    Add files via upload

---
+++ test automation/CreateAccount.js
@@ -0,0 +1,68 @@
+describe('Create Account', () => {
+	it('Navigate to Create Account page', () => {
+    	cy.visit('http://10.147.20.66/frontend/createAccount.html')
+    	cy.contains('Sign Up')
+  	})
+
+
+  	it('Trying to submit w/o info', () => {
+      cy.get('.signupbtn').click()
+      cy.url().should('not.equal','http://10.147.20.66/frontend/login.html')
+    })
+
+    it('Pressing cancel button', () => {
+      cy.get('.cancelbtn').click()
+      cy.url().should('equal','http://10.147.20.66/frontend/login.html')
+    })
+
+    it('Renavigating back to Create Account page', () => {
+  		cy.get('.medium-editor-element').click()
+  		cy.url().should('eq', 'http://10.147.20.66/frontend/createAccount.html')
+  	})
+
+	it('Typing in info', () => {
+  		cy.get('.username')
+  		// .type() with special character sequences
+      .type('{leftarrow}{rightarrow}{uparrow}{downarrow}')
+      .type('{del}{selectall}{backspace}')
+
+      // .type() with key modifiers
+      .type('{alt}{option}') //these are equivalent
+      .type('{ctrl}{control}') //these are equivalent
+      .type('{meta}{command}{cmd}') //these are equivalent
+      .type('{shift}')
+      //typing in username
+      .type('random@gmail.com')
+
+	  	cy.get('.password')
+  		// .type() with special character sequences
+      .type('{leftarrow}{rightarrow}{uparrow}{downarrow}')
+      .type('{del}{selectall}{backspace}')
+
+      // .type() with key modifiers
+      .type('{alt}{option}') //these are equivalent
+      .type('{ctrl}{control}') //these are equivalent
+      .type('{meta}{command}{cmd}') //these are equivalent
+      .type('{shift}')
+      //typing in password
+      .type('asdf')
+
+      cy.get('.repeat')
+  		// .type() with special character sequences
+      .type('{leftarrow}{rightarrow}{uparrow}{downarrow}')
+      .type('{del}{selectall}{backspace}')
+
+      // .type() with key modifiers
+      .type('{alt}{option}') //these are equivalent
+      .type('{ctrl}{control}') //these are equivalent
+      .type('{meta}{command}{cmd}') //these are equivalent
+      .type('{shift}')
+      //typing in password
+      .type('asdf')
+  	})
+
+  	it('Submit with info', () => {
+      cy.get('.signupbtn').click()
+      cy.url().should('equal','http://10.147.20.66/frontend/homepage.html')
+    })
+})
